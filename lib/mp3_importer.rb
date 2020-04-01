79c0e0b5ad58f651c9d57f583152e200a705d00c
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-01 19:02:16 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,6 +1,15 @@
 class MP3Importer
-  
-  def import(list_of_filenames)
-    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
-  end 
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir[@path+"/*.mp3"].map { |file| file.split("/").last }
+  end
+
+  def import
+    files.each { |file| Song.new_by_filename(file) }
+  end
 end 
