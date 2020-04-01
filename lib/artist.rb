a28a35c7385fcc37f8997acbdc6cf7c0698b127a
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-01 18:18:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,7 @@ class Artist
   end 
   
   def self.find_or_create_by_name(name)
-    if name == self.name
+    if name == self.name?
       self.find_by_name(name)
     else
       self.create_by_name(name)
