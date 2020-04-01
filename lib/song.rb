d71b09d2222103f44dec9bc8e59fec3867a50d79
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-01 19:01:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,33 +1,21 @@
-class Song 
+class Song
   attr_accessor :name, :artist
-  
-  @@all = []
-  
-  def self.all 
-    @@all 
-  end 
-  
-  def save 
-    @@all << self
-  end 
-  
+
   def initialize(name)
     @name = name
-    @@all << self
-  end 
-  
-  def self.new_by_filename(filename)
-    song_name = filename.split("- ")[1].chomp(".mp3")
-    artist_name = filename.split("-")[0].chomp(".mp3")
-    song = Song.new(name)
-    song.name = song_name 
-    artist.name = artist_name
-    song 
-  end 
-  
+  end
+
+  def self.new_by_filename(file)
+    artist_name = file.split(" - ")[0]
+    song_name = file.split(" - ")[1]
+    song = Song.new(song_name)
+    song.artist_name = artist_name
+    song
+  end
+
   def artist_name=(name)
     artist = Artist.find_or_create_by_name(name)
-    artist.name = artist
+    self.artist = artist
     artist.add_song(self)
   end
-end
+end 
