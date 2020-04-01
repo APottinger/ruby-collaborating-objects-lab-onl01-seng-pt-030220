29432aa0ad2ffd5a65141de1a2008ed1f48ea33e
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-01 19:02:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,48 +1,45 @@
-class Artist 
-  attr_accessor :name, :artist, :songs 
-  
+class Artist
+  attr_accessor :name, :songs
+
   @@all = []
-  
-  def self.all 
-    @@all 
-  end 
-  
+
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self
-  end 
-  
-  def add_song(name)
-    @songs << name
-  end 
-  
-  def self.find_by_name(name)
-    self.all.detect {|artist| artist.name == name}
-  end 
-  
-  def self.create_by_name(name)
-    artist = Artist.new(name) 
-  end 
-  
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.name = name
+    artist
+  end
+
+  def self.find(name)
+    self.all.detect { |artist| artist.name == name }
+  end
+
   def self.find_or_create_by_name(name)
-    if self.name == name?
-      self.find_by_name(name)
-    else
-      self.create_by_name(name)
-    end
-  end 
-  
+    self.find(name) || self.create(name)
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
   def print_songs
-    self.songs.each{ |song| puts song.name }
-  end 
+    songs.each { |song| puts song.name }
+  end
+
+  def save
+    @@all << self
+  end
+
+  def songs
+    @songs
+  end
 end 
-mj = Artist.new("Michael Jackson")
-
-
-mj.print_songs
-
-Artist.create_by_name("aj")
-  
-  
-  
